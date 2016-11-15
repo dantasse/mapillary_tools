a0b47fd247fed94f0487e0996090dc472a1128c7
Dan Tasse <dan.tasse@gmail.com>
2016-11-15 14:14:02 -0800

    make it work for a whole directory

--- python/download_images.py
+++ python/download_images.py
@@ -2,16 +2,16 @@ import urllib2, urllib
 import json
 import os
 import shutil
-import argparse
+import argparse, geojson, shapely.geometry
 
 MAPILLARY_API_IM_SEARCH_URL = 'https://a.mapillary.com/v1/im/search?'
-BASE_DIR = 'downloaded/'
 
 
 '''
 Script to download images using the Mapillary image search API.
 
-Downloads images inside a rect (min_lat, max_lat, min_lon, max_lon).
+Downloads images for each neighborhood within a city. (specified by a geojson
+file.)
 '''
 
 def create_dirs(base_path):
@@ -19,7 +19,7 @@ def create_dirs(base_path):
         shutil.rmtree(base_path)
     except:
         pass
-    os.mkdir(base_path)
+    os.makedirs(base_path)
 
 
 def query_search_api(min_lat, max_lat, min_lon, max_lon, max_results):
@@ -65,24 +65,49 @@ if __name__ == '__main__':
     '''
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('min_lat', type=float)
-    parser.add_argument('max_lat', type=float)
-    parser.add_argument('min_lon', type=float)
-    parser.add_argument('max_lon', type=float)
-    parser.add_argument('--max_results', default=400)
+    parser.add_argument('--city_name', default='pgh')
+    parser.add_argument('--max_results', default=1000)
+    parser.add_argument('--neighborhoods_file', default='nghd_bounds.geojson')
     parser.add_argument('--image_size', type=int, default=1024, choices=[320,640,1024,2048])
     args = parser.parse_args()
 
-    # query api
-    query = query_search_api(args.min_lat, args.max_lat, args.min_lon, args.max_lon, args.max_results)
+    nghds = geojson.load(open(args.neighborhoods_file))['features']
+    for nghd in nghds:
+        nghd_name = nghd['properties']['name']
+        nghd_shape = shapely.geometry.asShape(nghd['geometry'])
+        min_lon, min_lat, max_lon, max_lat = nghd_shape.bounds
 
-    # create directories for saving
-    create_dirs(BASE_DIR)
+        # query api
+        query = query_search_api(min_lat, max_lat, min_lon, max_lon, args.max_results)
+        if len(query) == args.max_results:
+            pass # TODO do something b/c you might be missing some.
 
-    # download
-    downloaded_list = download_images(query, path=BASE_DIR, size=args.image_size)
+        nghd_dir = args.city_name + "/" + nghd_name + "/"
+        # create directories for saving
+        create_dirs(nghd_dir)
 
-    # save filename with lat, lon
-    with open(BASE_DIR+"downloaded.txt", "w") as f:
-        for data in downloaded_list:
-            f.write(",".join(data) + "\n")
+        # download
+        # downloaded_list = download_images(query, path=nghd_dir, size=args.image_size)
+        im_size = "thumb-{0}.jpg".format(args.image_size)
+        downloaded_list = []
+
+        for im in query:
+            if shapely.geometry.Point(im['lon'], im['lat']).within(nghd_shape):
+                url = im['image_url']+im_size
+                filename = im['key']+".jpg"
+                try:
+                    print nghd_dir+filename
+                    image = urllib.URLopener()
+                    image.retrieve(url, nghd_dir+filename)
+                    downloaded_list.append([filename, str(im['lat']), str(im['lon'])])
+                    print("Successfully downloaded: {0}".format(filename))
+                except KeyboardInterrupt:
+                    break
+                except:
+                    print("Failed to download: {0}".format(filename))
+
+
+        # save filename with lat, lon
+        with open(nghd_dir+"downloaded.txt", "w") as f:
+            for data in downloaded_list:
+                f.write(",".join(data) + "\n")
